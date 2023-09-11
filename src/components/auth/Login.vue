0bb938c07efb7738bd98ff8a04bf2860e7cd1c22
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-11 15:55:04 +0500

    add register

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -4,12 +4,25 @@
             <form class="col-4 m-auto">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <input
+                    type="email"
+                    v-model="email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    />
+                    <div id="emailHelp" class="form-text">
+                        We'll never share your email with anyone else.
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input
+                    type="password"
+                    v-model="password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    />
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
@@ -17,10 +30,30 @@
     </div>
 </template>
 <script>
+import axios from "@/sevice/axios";
 export default {
-    name: 'Login'
-}
+    name: "Login",
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await axios.post("/auth/login", {
+                    email: this.email,
+                    password: this.password,
+                });
+                localStorage.setItem("access_token", response.data.access_token);
+            } catch {
+                console.log('xato');
+            }
+            this.email = '';
+            this.password = '';
+        },
+    },
+};
 </script>
-<style>
-
-</style>
+<style></style>
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,30 +1,72 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <form class="col-4 m-auto">
-                <div class="mb-3">
-                    <label for="exampleInputName1" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="exampleInputName1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+  <div class="container">
+    <div class="row">
+      <form class="col-4 m-auto">
+        <div class="mb-3">
+          <label for="exampleInputName1" class="form-label">Name</label>
+          <input type="text" v-model="name" class="form-control" id="exampleInputName1" />
         </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input
+            type="email"
+            v-model="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input
+            type="password"
+            v-model="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <button
+          type="submit"
+          @click.prevent="store"
+          :disabled="disabl"
+          class="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
     </div>
+  </div>
 </template>
 <script>
+import axios from "@/sevice/axios";
 export default {
-    name: 'Register'
-}
+  name: "Register",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      disabl: false,
+    };
+  },
+  methods: {
+    async store() {
+      this.disabl = true;
+      await axios
+        .post("/register", {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          alert(res.data.message);
+        });
+      (this.disabl = false),(this.name = ""), (this.email = ""), (this.password = "");
+    },
+  },
+};
 </script>
-<style>
-    
-</style>
+<style></style>
